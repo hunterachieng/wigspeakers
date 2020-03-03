213373f76b6a7e93382e6ba95dae746386516c34
Aida Monfort Muriach <aida.monfort@uji.es>
2020-03-03 15:12:27 +0100

    hide firebase keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCMjlZxtKdMe9I2HLelUbUJjBg-dPDDhBc',
-    authDomain: 'speakerswig.firebaseapp.com',
-    databaseURL: 'https://speakerswig.firebaseio.com',
-    projectId: 'speakerswig',
-    storageBucket: 'speakerswig.appspot.com',
-    messagingSenderId: '109635876635',
-    appId: '1:109635876635:web:3d9c5c0c0ed0d5411745be',
-    measurementId: 'G-VM7RBFH9FL'
+
   }
 };
