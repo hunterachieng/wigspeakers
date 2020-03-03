bbee84f3282d698dc03877aeeaa64702a324f9d2
Aida Monfort Muriach <aida.monfort@uji.es>
2020-03-03 15:05:37 +0100

    fix multiselect inputs

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -15,82 +15,82 @@ export class RegisterComponent implements OnInit {
   user: User = null;
   public model: Speaker;
 
-  public otherArea = '';
+  public otherAreaAdd = '';
+  public otherAppAdd = '';
+
+  public errorGDPR = '';
+  public savedTxt = '';
+
+  public acceptedGDPR = false;
 
   public areas = [
-    'Research / Science',
-    // 'Please specify ...',
-    'Geospatial Software',
-    'FOSS4G(e.g.QGIS, gdal)',
-    'ArcGIS Suite',
-    'Mapinfo Suite',
-    'Cadcorp Suite',
-    'FME',
-    'Other(specify)',
-    'Webmapping',
-    'Open Layers',
-    'Leaflet',
-    'ArcGIS Suite',
-    'D3',
-    'Mapbox Studio and Mapbox GL',
-    'Other(Specify)',
-    'Geospatial Open Data platforms',
-    'Geonode / Carto',
-    'ArcGIS Suite',
-    'Remote Sensing & Earth Observations',
-    'Geographic Information Systems',
-    'Ethical Geography',
-    'Geospatial cloud platforms',
-    'Google Earth Engine',
-    'Amazon Web Services',
-    'Other(Specify)',
-    ' (Geospatial) programming / Data Science',
-    'Python',
-    'R',
-    'Jupyter Notebooks',
-    'Other(Specify)',
-    'Domain - specific geospatial applications',
-    'Public Health',
-    'Defence and Security',
-    'Emergency Service / Public Safety',
-    'Climate',
-    'Defence and Security',
-    'Smart Cities',
-    'Citizen Science',
-    'Transportation',
-    'Energy',
-    'Manufacturing',
-    'Environment',
-    'Food, Forestry, Agriculture',
-    'Sustainable Development',
-    'Other(Please specify …)',
-    'Data Visualisation',
-    'Cartography',
-    'Dashboards',
-    'Graphic design',
-    ' (Geospatial) Data Journalism',
-    'Strategic Skills',
-    'Geospatial leadership',
-    'Policy',
-    'GI Implementation / Strategy',
-    'Growth in the geospatial / space Industry',
-    'Geospatial Data',
-    'Spatial Data Analysis & Insight',
-    'Location Intelligence',
-    'Big Data / Geospatial Data',
-    'Open Data',
-    'Entrepreneurship',
-    'Innovation / New Uses of GIS data',
-    'Augmented Reality(AR)',
-    'Virtual Reality(VR)',
-    'Machine Learning / Artificial Intelligence',
-    'Blockchain',
-    '5G',
-    'Internet - of - Things(IoT)',
-    'Geospatial and digital transformation',
-    'Other '
+    { name: 'Please specify ...', group: 'Research / Science', full: 'Research / Science' },
+    { name: 'FOSS4G(e.g.QGIS, gdal)', group: 'Geospatial Software', full: 'FOSS4G(e.g.QGIS, gdal) -> Geospatial Software' },
+    { name: 'ArcGIS Suite', group: 'Geospatial Software', full: 'ArcGIS Suite -> Geospatial Software' },
+    { name: 'Mapinfo Suite', group: 'Geospatial Software', full: 'Mapinfo Suite -> Geospatial Software' },
+    { name: 'Cadcorp Suite', group: 'Geospatial Software', full: 'Cadcorp Suite -> Geospatial Software' },
+    { name: 'FME', group: 'Geospatial Software', full: 'FME -> Geospatial Software' },
+    { name: 'Other(specify)', group: 'Geospatial Software', full: 'Other(specify) -> Geospatial Software' },
+    { name: 'Open Layers', group: 'Webmapping', full: 'Open Layers -> Webmapping' },
+    { name: 'Leaflet', group: 'Webmapping', full: 'Leaflet -> Webmapping' },
+    { name: 'ArcGIS Suite', group: 'Webmapping', full: 'ArcGIS Suite -> Webmapping' },
+    { name: 'D3', group: 'Webmapping', full: 'D3 -> Webmapping' },
+    { name: 'Mapbox Studio and Mapbox GL', group: 'Webmapping', full: 'Mapbox Studio and Mapbox GL -> Webmapping' },
+    { name: 'Other(Specify)', group: 'Webmapping', full: 'Other(Specify) -> Webmapping' },
+    { name: 'Geonode / Carto', group: 'Geospatial Open Data platforms', full: 'Geonode / Carto -> Geospatial Open Data platforms' },
+    { name: 'ArcGIS Open Data Platform', group: 'Geospatial Open Data platforms', full: 'ArcGIS Open Data Platform -> Geospatial Open Data platforms' },
+    { name: 'Copernicus Open Data', group: 'Geospatial Open Data platforms', full: 'Copernicus Open Data -> Geospatial Open Data platforms' },
+    { name: 'Earth on AWS', group: 'Geospatial Open Data platforms', full: 'Earth on AWS -> Geospatial Open Data platforms' },
+    { name: 'Google Open Data initiative', group: 'Geospatial Open Data platforms', full: 'Google Open Data initiative -> Geospatial Open Data platforms' },
+    { name: 'Remote Sensing & Earth Observations', group: 'EO', full: 'Remote Sensing & Earth Observations' },
+    { name: 'Geographic Information Systems', group: 'GI', full: 'Geographic Information Systems' },
+    { name: 'Ethical Geography', group: '', full: 'Ethical Geography' },
+    { name: 'Google Earth Engine', group: 'Geospatial cloud platforms', full: 'Google Earth Engine -> Geospatial cloud platforms' },
+    { name: 'Amazon Web Services', group: 'Geospatial cloud platforms', full: 'Amazon Web Services -> Geospatial cloud platforms' },
+    { name: 'Other(Specify)', group: 'Geospatial cloud platforms', full: 'Other(Specify) -> Geospatial cloud platforms' },
+    { name: 'Python', group: '(Geospatial) programming / Data Science', full: 'Python -> (Geospatial) programming / Data Science' },
+    { name: 'R', group: '(Geospatial) programming / Data Science', full: 'R -> (Geospatial) programming / Data Science' },
+    { name: 'Jupyter Notebooks', group: '(Geospatial) programming / Data Science', full: 'Jupyter Notebooks -> (Geospatial) programming / Data Science' },
+    { name: 'Javascript', group: '(Geospatial) programming / Data Science', full: 'Javascript -> (Geospatial) programming / Data Science' },
+    { name: 'Other(Specify)', group: '(Geospatial) programming / Data Science', full: 'Other(Specify) -> (Geospatial) programming / Data Science' },
+    { name: 'Cartography', group: 'Data Visualisation', full: 'Cartography -> Data Visualisation' },
+    { name: 'Dashboards', group: 'Data Visualisation', full: 'Dashboards -> Data Visualisation' },
+    { name: 'Graphic design', group: 'Data Visualisation', full: 'Graphic design -> Data Visualisation' },
+    { name: '(Geospatial) Data Journalism', group: '', full: '(Geospatial) Data Journalism' },
+    { name: 'Geospatial leadership', group: 'Strategic Skills', full: 'Geospatial leadership -> Strategic Skills' },
+    { name: 'Policy', group: 'Strategic Skills', full: 'Policy -> Strategic Skills' },
+    { name: 'GI Implementation / Strategy', group: 'Strategic Skills', full: 'GI Implementation / Strategy -> Strategic Skills' },
+    { name: 'Growth in the geospatial / space Industry', group: 'Strategic Skills', full: 'Growth in the geospatial / space Industry -> Strategic Skills' },
+    { name: 'Spatial Data Analysis & Insight', group: 'Geospatial Data', full: 'Spatial Data Analysis & Insight -> Geospatial Data' },
+    { name: 'Location Intelligence', group: 'Geospatial Data', full: 'Location Intelligence -> Geospatial Data' },
+    { name: 'Big Data / Geospatial Data', group: 'Geospatial Data', full: 'Big Data / Geospatial Data -> Geospatial Data' },
+    { name: 'Open Data', group: 'Geospatial Data', full: 'Open Data -> Geospatial Data' },
+    { name: 'Entrepreneurship', group: '', full: 'Entrepreneurship' },
+    { name: 'Augmented Reality (AR)', group: 'Innovation / New Uses of GIS data', full: 'Augmented Reality (AR) -> Innovation / New Uses of GIS data' },
+    { name: 'Virtual Reality (VR)', group: 'Innovation / New Uses of GIS data', full: 'Virtual Reality (VR) -> Innovation / New Uses of GIS data' },
+    { name: 'Machine Learning / Artificial Intelligence', group: 'Innovation / New Uses of GIS data', full: 'Machine Learning / Artificial Intelligence -> Innovation / New Uses of GIS data' },
+    { name: 'Blockchain', group: 'Innovation / New Uses of GIS data', full: 'Blockchain -> Innovation / New Uses of GIS data' },
+    { name: '5G', group: 'Innovation / New Uses of GIS data', full: '5G -> Innovation / New Uses of GIS data' },
+    { name: 'Internet-of-Things (IoT)', group: 'Innovation / New Uses of GIS data', full: 'Internet-of-Things (IoT) -> Innovation / New Uses of GIS data' },
+    { name: 'Geospatial and digital transformation', group: 'Innovation / New Uses of GIS data', full: 'Geospatial and digital transformation -> Innovation / New Uses of GIS data' },
+    { name: 'Other', group: '', full: 'Other' }
   ];
 
+  public applications = [
+    { name: 'Public Health', group: '' },
+    { name: 'Defence and Security', group: '' },
+    { name: 'Emergency Service / Public Safety', group: '' },
+    { name: 'Climate', group: '' },
+    { name: 'Smart Cities', group: '' },
+    { name: 'Citizen Science', group: '' },
+    { name: 'Transportation', group: '' },
+    { name: 'Energy', group: '' },
+    { name: 'Manufacturing', group: '' },
+    { name: 'Environment', group: '' },
+    { name: 'Food, Forestry, Agriculture', group: '' },
+    { name: 'Sustainable Development', group: '' },
+    { name: 'Other(Please specify …)', group: '' }
+  ];
 
   public languages = [
     'Afrikaans',
@@ -167,6 +167,26 @@ export class RegisterComponent implements OnInit {
     'Xhosa'
   ];
 
+  public region = [
+    'Africa - West',
+    'Africa - East',
+    'Africa - Other',
+    'Asia - China',
+    'Asia - India',
+    'Asia - Other',
+    'Australia / New Zealand',
+    'Canada',
+    'Central America',
+    'US - East Coast',
+    'US - West Coast',
+    'US - South',
+    'US - Midwest',
+    'Latin America',
+    'Middle East',
+    'Europe - Other',
+    'Europe - UK/ Ireland ',
+  ];
+
   constructor(public fAuth: AngularFireAuth, public fFire: AngularFirestore) {
     this.model = new Speaker();
     this.fAuth.auth.onAuthStateChanged(u => {
@@ -185,19 +205,42 @@ export class RegisterComponent implements OnInit {
   }
 
   saveData() {
-    console.log(this.model);
-    this.fFire.collection('speakers').doc<Speaker>(this.user.uid).set(Object.assign({}, this.model));
+    if (this.acceptedGDPR) {
+      console.log(this.model);
+      this.fFire.collection('speakers').doc<Speaker>(this.user.uid).set(Object.assign({}, this.model));
+      this.errorGDPR = '';
+      this.savedTxt = 'Your data has been saved!';
+    } else {
+      this.errorGDPR = 'Yo need to accept our GDPR statement and Terms and Conditions.';
+      this.savedTxt = '';
+    }
   }
 
   ngOnInit(): void {
   }
 
-  clickButton() {
-    console.log("BUTTON PRESSED");
-  }
-
   logOut() {
     this.fAuth.auth.signOut();
+  }
+
+  addOtherApp() {
+    if (this.otherAppAdd != '') {
+      if (!this.model.otherApplications) {
+        this.model.otherApplications = [];
+      }
+      this.model.otherApplications.push(this.otherAppAdd);
+      this.otherAppAdd = '';
+    }
+  }
+
+  addOtherArea() {
+    if (this.otherAreaAdd != '') {
+      if (!this.model.otherAreas) {
+        this.model.otherAreas = [];
+      }
+      this.model.otherAreas.push(this.otherAreaAdd);
+      this.otherAreaAdd = '';
+    }
   }
 
 }
