4d9e98f5efeeb6e848abf9a58fd5a36f313fdac3
Aida Monfort Muriach <aida.monfort@uji.es>
2020-03-04 07:50:19 +0100

    forgot password and FB login works

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCMjlZxtKdMe9I2HLelUbUJjBg-dPDDhBc',
-    authDomain: 'speakerswig.firebaseapp.com',
-    databaseURL: 'https://speakerswig.firebaseio.com',
-    projectId: 'speakerswig',
-    storageBucket: 'speakerswig.appspot.com',
-    messagingSenderId: '109635876635',
-    appId: '1:109635876635:web:3d9c5c0c0ed0d5411745be',
-    measurementId: 'G-VM7RBFH9FL'
+
   }
 };
 
