ee571ea3523c5e7bc515f2f9e0f45ec86b8bd5bc
Aida Monfort Muriach <aida.monfort@uji.es>
2020-05-09 10:17:39 +0200

    display geo pin in map

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -25,7 +25,48 @@ export class MapService {
       center: [this.lng, this.lat]
     });
     this.map.addControl(new mapboxgl.NavigationControl());
+
     console.log('build map');
+
+    this.map.on('load', () => {
+      this.map.loadImage(
+        '../assets/img/pin.png',
+        (error, image) => {
+          if (error) {
+            throw error;
+          }
+          this.map.addImage('geowigs', image);
+          this.map.addSource('point', {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: [
+                {
+                  type: 'Feature',
+                  geometry: {
+                    type: 'Point',
+                    coordinates: [0, 0]
+                  }
+                }
+              ]
+            }
+          });
+          this.map.addLayer({
+            id: 'points',
+            type: 'symbol',
+            source: 'point',
+            layout: {
+              'icon-image': 'geowigs',
+              'icon-size': 0.25
+            }
+          });
+        }
+      );
+    });
+
+
+
+
 
   }
 }
