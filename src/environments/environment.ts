92e52b6559fe201eab389c430d2e912bc5f8e746
hunterachieng <achienghunter@gmail.com>
2021-10-27 08:24:10 +0300

    registration changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,17 +6,26 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCMjlZxtKdMe9I2HLelUbUJjBg-dPDDhBc',
-    authDomain: 'speakerswig.firebaseapp.com',
-    databaseURL: 'https://speakerswig.firebaseio.com',
-    projectId: 'speakerswig',
-    storageBucket: 'speakerswig.appspot.com',
-    messagingSenderId: '109635876635',
-    appId: '1:109635876635:web:3d9c5c0c0ed0d5411745be',
-    measurementId: 'G-VM7RBFH9FL'
+    apiKey: 'AIzaSyDgBNJr31TYbVJDXZlj7cTiKBCnSSoJwzk',
+    authDomain: 'womenintransportprofessionals.firebaseapp.com',
+    databaseURL: 'https://womenintransportprofessionals-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: 'womenintransportprofessionals',
+    storageBucket: 'womenintransportprofessionals.appspot.com',
+    messagingSenderId: '690511770041',
+    // appId: '1:109635876635:web:3d9c5c0c0ed0d5411745be',
+    // measurementId: 'G-VM7RBFH9FL'
   }
 };
-
+// firebase: {
+//   apiKey: 'AIzaSyCMjlZxtKdMe9I2HLelUbUJjBg-dPDDhBc',
+//   authDomain: 'speakerswig.firebaseapp.com',
+//   databaseURL: 'https://speakerswig.firebaseio.com',
+//   projectId: 'speakerswig',
+//   storageBucket: 'speakerswig.appspot.com',
+//   messagingSenderId: '109635876635',
+//   appId: '1:109635876635:web:3d9c5c0c0ed0d5411745be',
+//   measurementId: 'G-VM7RBFH9FL'
+// }
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
